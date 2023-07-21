4000a72f74ab7b9dfe111a9e619397582971c23d
kirtondavid53 <kirtondavid53@gmail.com>
2023-07-20 20:44:17 -0400

    add detail page

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("movie_details/<str:pk>", views.movie_details, name="movie_details")
 ]
--- core/views.py
+++ core/views.py
@@ -4,3 +4,6 @@ from django.conf import settings
 def index(request):
     name = 'David'
     return render(request, 'index.html', {'name':name, 'ACCESS_TOKEN':settings.ACCESS_TOKEN})
+
+def movie_details(request, pk):
+    return render(request, 'movie_details.html', {'movie_id':pk, 'ACCESS_TOKEN':settings.ACCESS_TOKEN})
